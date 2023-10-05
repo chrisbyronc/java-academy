d281cb74e7d40c2ed7d6472e063776e26abccb65
Chris <chrisbyronc@gmail.com>
2023-10-05 09:55:14 -0400

    adding mathapp 2-5

--- week-1/class/day-3/MathApp/src/main/java/com/pluralsight/MathApp.java
+++ week-1/class/day-3/MathApp/src/main/java/com/pluralsight/MathApp.java
@@ -10,5 +10,63 @@ public class MathApp {
         double highestSalary = Math.max(bobSalary, garySalary);
 
         System.out.println("The highest salary is: $" + highestSalary);
+
+        // Question 2
+        // Find and display the smallest of two variables named carPrice and truckPrice.
+        // Set the variables to any value you want.
+
+        double carPrice = 12000.00;
+        double truckPrice = 36000.00;
+
+        System.out.println("The lowest car price: " + Math.min(carPrice, truckPrice));
+
+
+
+        // Question 3
+        //  Find and display the area of a circle whose radius is 7.25
+
+
+        double radius = 7.25;
+        double area = Math.PI * Math.pow(radius , 2);
+        System.out.println("The area of a circle with a radius of " + radius + " is " + area);
+
+
+
+        //Question 4
+        // Find and display the square root a variable after it is set to 5.0
+
+        double number = 5.0;
+        double sqrtValue = Math.sqrt(5.0);
+
+        System.out.println("sqrt of " + number + " is " + sqrtValue);
+
+
+
+//        Question 5
+//        Find and display the distance between the points (5, 10) and (85, 50)
+
+        int x1 = 5;
+        int y1 = 10;
+        int x2 = 85;
+        int y2 = 50;
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("The distance between some points "+ distance);
+
+
+
+
+
+//        Question 6
+//        Find and display the absolute (positive) value of a variable after it is set to -3.8
+
+//        Question 7
+//        Find and display a random number between 0 and 1
+
+
+
+
+
+
     }
 }
---
+++ week-1/day-4-objectives.md
@@ -0,0 +1,67 @@
+What did you learn yesterday?
+
+1. cases
+   1. UpperCamelCase - Pascal
+   2. lowerCamelCase
+   3. snake_case
+2. Syntax
+   1. java is case sensitive
+   2. 7. Semicolon to terminate line
+   3. blocks can be { }
+   4. comments - Notes, descriptive lines
+      1. //comment 1 - line based comment
+      2. /* block based comment or Multi line */
+3. math in java
+   1. + - * /
+   2. % - remainder
+   3. variable++ - ++ adds 1
+   4. variable-- - -- subtracts 1
+   5. variable *= 10 - operators= 
+4. Math class
+   1. Math.max
+   2. Math.min
+   3. Math.abs
+   4. Math.sqrt
+   5. Math.Pi
+5. primitive data types
+   1. char - single character
+      1. single quotes
+   2. String
+      1. double quotes
+   3. boolean - true, false
+   4. numerical
+      1. int
+         1. "100" - default value is int
+      2. double
+         1. "100.00" - default value for decimal numbers
+      3. float
+      4. byte
+      5. short long
+6. Variables - containers, reference values
+   1. final - constant, immutable value
+7. System.out.println
+8. concatenation
+   1. combining two or more strings
+9.  escaping vim
+   1. esc, shift+:, wq! enter
+
+Todays Objectives
+
+1. Scanning the screen
+2. Different types of printing to our System out
+3. Working with methods
+4. Conditionals
+
+
+Agenda Today
+
+1. Recap
+2. We do exercise on operations
+3. Scanner
+4. Break
+5. More scanner
+6. Lunch
+7. Methods
+8. Break
+9. Conditionals
+10. Recap and Q&A
