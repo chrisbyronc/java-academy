37cb656a4cc505c71bd58ff810fb3b9edf10d030
Chris <chrisbyronc@gmail.com>
2023-12-19 07:58:07 -0500

    day 2, moved day 1 demo

--- week-11/class/day-1/DemoSpringBootGeneration/src/main/java/com/pluralsight/DemoSpringBootGenerationApplication.java
+++ week-11/class/day-1/DemoSpringBootGeneration/src/main/java/com/pluralsight/DemoSpringBootGenerationApplication.java
@@ -1,13 +1,13 @@
-package com.pluralsight.demo;
+package com.pluralsight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoSpringBootGenerationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoSpringBootGenerationApplication.class, args);
 	}
 
 }
--- week-11/class/day-1/DemoSpringBootGeneration/src/test/java/com/pluralsight/DemoSpringBootGenerationApplicationTests.java
+++ week-11/class/day-1/DemoSpringBootGeneration/src/test/java/com/pluralsight/DemoSpringBootGenerationApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pluralsight.demo;
+package com.pluralsight;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoSpringBootGenerationApplicationTests {
 
 	@Test
 	void contextLoads() {
