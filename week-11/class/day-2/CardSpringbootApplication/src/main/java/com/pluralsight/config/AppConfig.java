7dc2cdc50ef1d0f3aca00e59e8b795a8d7bcdd93
Chris <chrisbyronc@gmail.com>
2023-12-19 13:23:32 -0500

    adding middle day day 2 week 11

--- week-11/class/day-2/CardSpringbootApplication/src/main/java/com/pluralsight/config/AppConfig.java
+++ week-11/class/day-2/CardSpringbootApplication/src/main/java/com/pluralsight/config/AppConfig.java
@@ -2,14 +2,18 @@ package com.pluralsight.config;
 
 import com.pluralsight.dao.CardDAO;
 import com.pluralsight.dao.CardDAOImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${datasource.username}")
+    private String username;
+
     @Bean
     public CardDAO cardDAO() {
-        return new CardDAOImpl();
+        return new CardDAOImpl(username);
     }
 }
--- week-11/class/day-2/CardSpringbootApplication/src/main/java/com/pluralsight/dao/CardDAOImpl.java
+++ week-11/class/day-2/CardSpringbootApplication/src/main/java/com/pluralsight/dao/CardDAOImpl.java
@@ -1,6 +1,5 @@
 package com.pluralsight.dao;
 
-import com.pluralsight.dao.CardDAO;
 import com.pluralsight.model.Card;
 
 import java.util.ArrayList;
@@ -12,10 +11,13 @@ public class CardDAOImpl implements CardDAO {
 
     private final Map<Integer, Card> cards = new HashMap<>();
 
-    public CardDAOImpl() {
+    public CardDAOImpl(String username) {
+        System.out.println("User signed in: " + username);
+
         cards.put(1, new Card("Clubs", "King", 10));
         cards.put(2, new Card("Spades", "Ace", 11));
         cards.put(3, new Card("Hearts", "Three", 3));
+        cards.put(4, new Card("Hearts", "Four", 4));
     }
 
     @Override
